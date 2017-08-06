63e6e7e348cf487c114d633d6ea9c77b22b27dcc
Jimmy  Carney <carrney@jimmys-mbp.attlocal.net>
2017-08-05 21:44:10 -0500

    overlay ficxed

--- LuckyDuck/EventCell.swift
+++ LuckyDuck/EventCell.swift
@@ -26,8 +26,8 @@ class EventCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
         /* black overlay  */
-        let overlay: UIView = UIView(frame: CGRect(x: 0, y: 0, width: self.picture.frame.size.width + 100, height:  self.picture.frame.size.height))
-        overlay.backgroundColor = UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.3)
+        let overlay: UIView = UIView(frame: CGRect(x: 0, y: 0, width: self.picture.frame.size.width + 100, height:  self.picture.frame.size.height + 100))
+        overlay.backgroundColor = UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.2)
         self.picture.addSubview(overlay)
     }
 
